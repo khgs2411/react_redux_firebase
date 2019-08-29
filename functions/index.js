319129f88c1653052a5d6e97ce1d5e49fb554520
Liad-PC <khgs2411@gmail.com>
2019-08-30 02:42:17 +0300

    removed hello world

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,9 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   response.send("Hello from Firebase!");
+// });
 
 const createNotification = notification => {
   return admin
