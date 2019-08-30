6e6aaeb5317877b4006cf9bcd8b5bce957f08246
Liad-PC <khgs2411@gmail.com>
2019-08-30 14:04:54 +0300

    Update SignUp.jsx
    Cleanup

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { signUp } from "../../store/actions/authActions";
-import { Form, Container, Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 class SignUp extends Component {
   state = {
     email: "",
@@ -24,39 +24,6 @@ class SignUp extends Component {
     const { auth, authError } = this.props;
     if (auth.uid) return <Redirect to="/" />;
     return (
-      // <div>
-      //   <div className="container">
-      //     <form onSubmit={this.handleSubmit} className="white">
-      //       <h5 className="grey-text text-darken-3">Sign Up</h5>
-      //       <div className="input-field">
-      //         <label htmlFor="email">Email</label>
-      //         <input type="email" id="email" onChange={this.handleChange} />
-      //       </div>
-      //       <div className="input-field">
-      //         <label htmlFor="password">Password</label>
-      //         <input
-      //           type="password"
-      //           id="password"
-      //           onChange={this.handleChange}
-      //         />
-      //       </div>
-      //       <div className="input-field">
-      //         <label htmlFor="lastName">Last Name</label>
-      //         <input type="text" id="lastName" onChange={this.handleChange} />
-      //       </div>
-      //       <div className="input-field">
-      //         <label htmlFor="firstName">First Name</label>
-      //         <input type="text" id="firstName" onChange={this.handleChange} />
-      //       </div>
-      //       <div className="input-field">
-      //         <button className="btn pink lighten-1 z-depth-0">Register</button>
-      //         <div className="red-text center">
-      //           {authError ? <p>{authError}</p> : null}
-      //         </div>
-      //       </div>
-      //     </form>
-      //   </div>
-      // </div>
       <Form onSubmit={this.handleSubmit}>
         <Form.Group>
           <Form.Label>Email address</Form.Label>
